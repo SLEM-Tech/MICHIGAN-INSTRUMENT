a38914868769c56193c726248629bce9350434a9
Christopher <ChristopherOhwavworua@transalliance.ng>
2025-07-29 23:08:17 +0100

    update

---
+++ src/app/browse/page.tsx
@@ -0,0 +1,14 @@
+import AppLayout from '@src/components/AppLayout';
+import React from 'react'
+
+const page = () => {
+  return (
+    <AppLayout>
+      <div className="flex items-center justify-center h-screen text-2xl font-semibold">
+        Page content Loading...
+      </div>
+    </AppLayout>
+  );
+}
+
+export default page;
