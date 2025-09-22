914cc888ff1e710a6faf69c50f8a5034914efb37
Chrisohwa <ChristopherOhwavworua@transalliance.ng>
2025-09-22 15:39:31 +0100

    Update CheckoutSummarySection.tsx
    enable proceed to checkout button

--- src/components/PageFragments/CheckoutSummarySection.tsx
+++ src/components/PageFragments/CheckoutSummarySection.tsx
@@ -37,16 +37,16 @@ const CheckoutSummarySection = ({
         </h4>
       </div>
       <Link
-        // onClick={handleCheckoutClick}
-        onClick={(e) => {
+         onClick={handleCheckoutClick}
+{/*         onClick={(e) => {
           e.preventDefault(); // Prevent the link from navigating
-        }}
+        }} */}
         href={calculateSubtotal() === 0 ? "/" : "/checkout"}
         className={`flex w-full justify-center items-center py-3 mt-4 rounded-md text-white ${
           calculateSubtotal() === 0
             ? "bg-red-500/50"
             : "hover:bg-red-500 bg-red-500"
-        } transition font-bold text-sm sm:text-base cursor-not-allowed`}
+        } transition font-bold text-sm sm:text-base cursor-pointer`}
       >
         {calculateSubtotal() === 0
           ? "Please select a product"
